4f2e11fc80977fb5f3b87918c331dee2dfef34fa
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 12:22:36 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-  name: "TransitionButton"
+    name: "TransitionButton",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS("8.0")
+    ],
+    products: [
+        .library(name: "TransitionButton", targets: ["TransitionButton"])
+    ],
+    targets: [
+        .target(
+            name: "TransitionButton",
+            path: "TransitionButton",
+            exclude: [
+                "Info.plist",
+            ],
+            resources: [.process("Resources")]
+        )
+    ]
 )
