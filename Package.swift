a4db883c77171880be0579053f5a0c90ac3048aa
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 15:22:52 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "TransitionButton",
     defaultLocalization: "en",
     platforms: [
-        .iOS("8.0")
+        .iOS("9.0")
     ],
     products: [
         .library(name: "TransitionButton", targets: ["TransitionButton"])
